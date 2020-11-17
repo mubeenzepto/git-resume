87e57c785f7fa42724e58fa94e5b88a30d12b9d9
mubeenzepto <mubeen@zeptosystems.com>
2020-11-17 12:51:28 +0500

    nav bar updated

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// nav bar scroll color
+$(function () {
+	$(document).scroll(function () {
+	  var $nav = $(".navbar-fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+  });
