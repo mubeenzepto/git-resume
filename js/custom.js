a2eae39bbb7fb83d1e1ff98e87a7df082ae83594
mubeenzepto <mubeen@zeptosystems.com>
2020-11-18 12:38:59 +0500

    markup improvements

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,26 @@ $(function () {
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
   });
+
+
+  // When the user scrolls down 80px from the top of the document, slide down the navbar
+// window.onscroll = function () {
+//     scrollFunction()
+// };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+//         document.getElementById("navbar").style.top = "0";
+//     } else {
+//         document.getElementById("navbar").style.display = "block";
+//         document.getElementById("navbar").style.top = "-85px";
+//     }
+// }
+
+// Bootstrap Scroll Spy
+$('body').scrollspy({
+    target: '#navbar'
+})
+$('[data-spy="scroll"]').each(function () {
+    var $spy = $(this).scrollspy('refresh')
+})
